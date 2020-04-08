aae4895e2f0a75d9223470da03ac34c9e29734e3
6009000134 <491675469@qq.com>
2020-04-08 09:09:05 +0800

    MES/u9 1、U9料品信息视图

--- Auctus_ItemmasterView.sql
+++ Auctus_ItemmasterView.sql
@@ -1,11 +1,12 @@
 
-CREATE VIEW [dbo].[Auctus_ItemmasterView]
+ALTER VIEW [dbo].[Auctus_ItemmasterView]
 AS
 
-SELECT a.ID,a.Code,a.Name,a.SPECS FROM dbo.CBO_ItemMaster a 
+SELECT a.ID,a.Code,a.Name,a.SPECS,a.DescFlexField_PrivateDescSeg18 UPPH FROM dbo.CBO_ItemMaster a 
 WHERE a.Org=1001708020135665
 AND a.Effective_IsEffective=1
 AND a.Effective_DisableDate>GETDATE()
 GO
+
 
 
