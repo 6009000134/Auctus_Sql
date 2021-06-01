fee23cb3f46ea0bdd9a24bfcc3a2a6453f68c661
6009000134 <491675469@qq.com>
2021-06-01 17:28:32 +0800

    工单包装报表增加旧SN码

--- MES/工单包装信息查询报表.sql
+++ MES/工单包装信息查询报表.sql
@@ -1,7 +1,7 @@
 /*
 工单包装信息查询报表
 */
-alter PROC sp_PackReport
+ALTER PROCEDURE [dbo].[sp_PackReport]
 (
 @WorkOrder VARCHAR(100)
 --@UserName varchar(100)
@@ -11,7 +11,7 @@ AS
 BEGIN 
 
 --DECLARE @WorkOrder VARCHAR(100),@UserName VARCHAR(100)
---SET @WorkOrder='3114'
+--SET @WorkOrder='MO-30190814024'
 SET @WorkOrder='%'+ISNULL(@WorkOrder,'')+'%'
 --SET @UserName='%'+ISNULL(@UserName,'')+'%'
 IF object_id('tempdb.dbo.#tempResult') is NULL
@@ -23,12 +23,11 @@ BEGIN
 	TRUNCATE TABLE #tempResult
 END
 INSERT INTO #tempResult
-SELECT  A.CreateDate,A.SNCode,
+SELECT     A.CreateDate,A.SNCode,
         B.PalletCode ,
         B.BoxNumber ,
         D.WorkOrder ,
         ISNULL(f.UserName,a.CreateBY)UserName
-		--,f.UserName,a.createby
 FROM    opPackageChild AS A
         INNER JOIN opPackageDetail AS B ON A.PackDetailID = B.ID
         INNER JOIN opPackageMain AS C ON C.ID = B.PackMainID
@@ -36,9 +35,24 @@ FROM    opPackageChild AS A
         LEFT JOIN dbo.syUser AS f ON f.LoginID = A.createBy
 WHERE   PATINDEX(@WorkOrder,D.WorkOrder)>0 --AND PATINDEX(@UserName,ISNULL(f.UserName,''))>0
 
-SELECT a.*,e.InternalCode FROM #tempResult a  INNER JOIN baInternalAndSNCode AS e ON e.SNCode = A.SNCode
-ORDER BY a.BoxNumber 
-END 
+;
+WITH data1 AS
+(
+SELECT g.OriInternalCode,g.OriSNCode,a.SNCode,g.InternalCode,0 lv FROM #tempResult a INNER JOIN 
+		(
+		SELECT InternalCode,SNCode FROM dbo.vw_baInternalAndSNCode 
+		UNION ALL 
+		SELECT InternalCode,SNCode1 FROM opInternalToSNCode
+		) f ON a.SNCode=f.SNCode
+		INNER JOIN opProductReworkNewBSN g ON f.InternalCode=g.InternalCode
+		UNION ALL
+        SELECT b.OriInternalCode,b.OriSNCode,a.SNCode,a.InternalCode,lv+1 FROM data1 a INNER JOIN opProductReworkNewBSN b ON a.OriInternalCode=b.InternalCode
+),
+oldData AS
+(
+SELECT *,ROW_NUMBER()OVER(PARTITION BY a.InternalCode ORDER BY lv desc)RN FROM data1 a
+)
+SELECT a.*,b.InternalCode,b.OriInternalCode,b.OriSNCode
+FROM #tempResult a INNER JOIN oldData b ON a.SNCode=b.SNCode
 
---SELECT * FROM #tempResult a
-
+END
