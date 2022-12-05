cf46dbc6da098c63c41d2f9f15239bd0be88c585
6009000134 <491675469@qq.com>
2022-12-05 18:03:39 +0800

    齐套sql

--- Q问题库/Supplier供应商交货计划推送.sql
+++ Q问题库/Supplier供应商交货计划推送.sql
@@ -1,10 +1,10 @@
 /*
-SELECT * INTO #TempTable8 FROM dbo.Auctus_FullSetCheckResult8 a WHERE a.CopyDate>'2022-11-14'
-DELETE FROM dbo.Auctus_FullSetCheckResult8  WHERE CopyDate>'2022-11-14'
+SELECT * INTO #TempTable8 FROM dbo.Auctus_FullSetCheckResult8 a WHERE a.CopyDate>'2022-12-05'
+DELETE FROM dbo.Auctus_FullSetCheckResult8  WHERE CopyDate>'2022-12-05'
 EXEC dbo.sp_Auctus_BackUp_FullSetCheckResult8
 */
 
 /*
-DELETE FROM dbo.Auctus_FullSetCheckResult8  WHERE CopyDate>'2022-11-14'
+DELETE FROM dbo.Auctus_FullSetCheckResult8  WHERE CopyDate>'2022-12-05'
 INSERT INTO Auctus_FullSetCheckResult8 SELECT * FROM #TempTable8
 */
